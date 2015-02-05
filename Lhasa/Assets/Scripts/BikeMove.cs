15ff7d1d1be5fb1861be5e698ad57e3a5f50b23b
jpsiyu <jpsiyu@163.com>
2015-02-05 03:19:12 -0500

    L*

---
+++ Lhasa/Assets/Scripts/BikeMove.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeMove : MonoBehaviour {
+
+	public float speed;
+	public float turnSpeed;
+	public float jumpPower;
+	public float jumpTime;
+
+	private float turnAngle = 20;	
+	private Vector3 jumpForce;
+	private bool isJumpping;
+
+	// Use this for initialization
+	void Start () {
+		isJumpping = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateBikePosition ();
+
+
+	}
+
+	void FixedUpdate(){
+		if(!isJumpping)
+			BikeJump ();
+	}
+
+	void UpdateBikePosition(){
+		Vector3 targetPosition = transform.position + transform.right * Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+		transform.position = targetPosition;
+		Vector3 targetEulerAngles =  transform.eulerAngles;
+		targetEulerAngles.x = -Input.GetAxis ("Horizontal") * turnAngle;
+		targetEulerAngles.y += Input.GetAxis ("Horizontal") * turnSpeed;
+		transform.eulerAngles = targetEulerAngles;
+	}
+
+	void BikeJump(){
+		if (Input.GetButtonDown ("Jump")) {
+			isJumpping = true;
+			jumpForce.Set(0.0f, jumpPower, 0.0f);
+			rigidbody.AddForce(jumpForce);
+			Invoke("DownForce", jumpTime);
+		}
+	}
+
+	void DownForce(){
+		isJumpping = false;
+		jumpForce.Set(0.0f, -jumpPower*1.5f, 0.0f);
+		rigidbody.AddForce(jumpForce);
+	}
+}
---
+++ Lhasa/Assets/Scripts/WheelRunning.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelRunning : MonoBehaviour {
+
+	private Vector3 newEulerAngles;
+
+	public float turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newEulerAngles = transform.eulerAngles;
+		newEulerAngles.z = newEulerAngles.z - turnSpeed;
+
+		transform.eulerAngles = newEulerAngles;
+
+		//print (transform.eulerAngles);
+	
+	}
+}
